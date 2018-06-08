c4ec7d74e7d8115c505dc51f1bfe9d1ab98802b6
Pablo Lucero <pablo.lucero@globallogic.com>
2018-06-07 20:13:08 -0500

    Di-Demo - Autowired example

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controller.ConstructorInjectedController;
 import guru.springframework.didemo.controller.MyController;
+import guru.springframework.didemo.controller.PropertyInjectedController;
+import guru.springframework.didemo.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,5 +16,9 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
 		controller.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/guru/springframework/didemo/controller/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controller/ConstructorInjectedController.java
@@ -1,9 +1,13 @@
 package guru.springframework.didemo.controller;
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
+    @Autowired
     private GreetingService greetingService;
 
     //Constructor Injected
